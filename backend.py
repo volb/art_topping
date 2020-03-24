14355ed955b5d8770816729d6f2c736969600e80
volb <700679+volb@users.noreply.github.com>
2020-03-23 17:48:35 -0700

    Add text file

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import os, sys
 from PIL import Image
 import urllib.request
 
-urllib.request.urlretrieve("http://www.digimouth.com/news/media/2011/09/google-logo.jpg", "local-filename.jpg")
+#urllib.request.urlretrieve("http://www.digimouth.com/news/media/2011/09/google-logo.jpg", "local-filename.jpg")
 
 f = open("albums.txt")
 
@@ -12,3 +12,5 @@ def save_images():
     for mbid in titles:
         image_template = f"https://coverartarchive.org/release-group/{mbid}/front-250.jpg"
         urllib.request.urlretrieve(image_template, f"{mbid},jpg")
+
+save_images()
