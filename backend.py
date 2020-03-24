439ad1db4f4e59ad90dc821bdc7fe7444763b543
volb <700679+volb@users.noreply.github.com>
2020-03-23 17:40:43 -0700

    Add function save images

--- backend.py
+++ backend.py
@@ -1,8 +1,14 @@
 import os, sys
 from PIL import Image
+import urllib.request
+
+urllib.request.urlretrieve("http://www.digimouth.com/news/media/2011/09/google-logo.jpg", "local-filename.jpg")
 
 f = open("albums.txt")
 
 titles = f.readlines()
 
-image_template = "https://coverartarchive.org/release-group/~A/front-250.jpg"
+def save_images():
+    for mbid in titles:
+        image_template = f"https://coverartarchive.org/release-group/{mbid}/front-250.jpg"
+        urllib.request.urlretrieve(image_template, f"{mbid},jpg")
